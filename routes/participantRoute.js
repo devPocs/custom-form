b741efdb5819a2c50327a9c5e1e6adee4d00b7fa
devPocs <pokoh.ufuoma@gmail.com>
2023-07-30 17:29:21 +0100

    made second corrections to the the image-type check

--- routes/participantRoute.js
+++ routes/participantRoute.js
@@ -18,21 +18,21 @@ const uploads = multer({ storage: storage });
 router.post(
 	"/",
 	uploads.single("file"),
-	(req, res, next) => {
-		if (
-			req.file.mimetype !== "application/jpeg" ||
-			req.file.mimetype !== "application/pdf" ||
-			req.file.mimetype !== "application/jpg" ||
-			req.file.mimetype !== "application/png"
-		) {
-			return res.status(400).json({
-				status: "fail",
-				message:
-					"file format not supported. Only pdf, jpeg, jpg, png files are supported"
-			});
-		}
-		next();
-	},
+	//(req, res, next) => {
+	//	if (
+	//		req.file.mimetype !== "application/jpeg" ||
+	//		req.file.mimetype !== "application/pdf" ||
+	//		req.file.mimetype !== "application/jpg" ||
+	//		req.file.mimetype !== "application/png"
+	//	) {
+	//		return res.status(400).json({
+	//			status: "fail",
+	//			message:
+	//				"file format not supported. Only pdf, jpeg, jpg, png files are supported"
+	//		});
+	//	}
+	//	next();
+	//},
 	checkNewParticipant,
 	checkEmail,
 	addParticipant
