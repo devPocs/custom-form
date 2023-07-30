912b9c5f35b439c1d31d5f3265024229ebf689b6
devPocs <pokoh.ufuoma@gmail.com>
2023-07-30 17:19:39 +0100

    made corrections to the the image-type check

--- routes/participantRoute.js
+++ routes/participantRoute.js
@@ -22,16 +22,14 @@ router.post(
 		if (
 			req.file.mimetype !== "application/jpeg" ||
 			req.file.mimetype !== "application/pdf" ||
-			req.file.mimetype !== "application/jpeg" ||
+			req.file.mimetype !== "application/jpg" ||
 			req.file.mimetype !== "application/png"
 		) {
-			return res
-				.status(400)
-				.json({
-					status: "fail",
-					message:
-						"file format not supported. Only pdf, jpeg, jpg, png files are supported"
-				});
+			return res.status(400).json({
+				status: "fail",
+				message:
+					"file format not supported. Only pdf, jpeg, jpg, png files are supported"
+			});
 		}
 		next();
 	},
