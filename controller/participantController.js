3851132628833b341c2a2824c15be2ec6a0ea0df
devPocs <pokoh.ufuoma@gmail.com>
2023-08-04 09:47:10 +0100

    made changes to the participant route. removed the mailer function

--- controller/participantController.js
+++ controller/participantController.js
@@ -6,15 +6,6 @@ const {
 	sanitizeTextInput
 } = require("./../utils/helperFunctions");
 
-//set up node mailer
-const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: "reg.nisgssouthsouth@gmail.com",
-		pass: "aejyropmzsizoewr"
-	}
-});
-
 exports.addParticipant = catchAsync(async (req, res, next) => {
 	const url = req.url;
 	let { name, email, phoneNumber, gender, educationalStatus, school, region } =
@@ -33,27 +24,7 @@ exports.addParticipant = catchAsync(async (req, res, next) => {
 	});
 	if (newParticipant) {
 		//send mail to the host
-		const mailOptions = {
-			from: "reg.nisgssouthsouth@gmail.com",
-			to: email,
-			subject: "Surveyor's Conference, 2023",
-			text: `<h2>Hello, ${name},</h2> <p>Thank you for registering for the conference.</p> 
-			<p>Your registration id is: <h2>${summitId}.</h2> 
-			<h3>Pls, be able to provide a printed copy of this info at the validation stand during the conference to get your conference tag.</h3>
-			<p>Thanks and warm regards.</p>`,
-
-			html: `<h2>Hello, ${name},</h2> <p>Thank you for registering for the conference.</p> 
-			<p>Your registration id is: <h2>${summitId}.</h2> 
-			<h3>Pls, be able to provide a printed copy of this info at the validation stand during the conference to get your conference tag.</h3> 
-			<p>Thanks and warm regards.</p>`
-		};
-		transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				res.status(500).send("Failed to send email");
-			} else {
-				res.status(200).json({ status: "success" });
-			}
-		});
+		res.status(200).json({ status: "success" });
 	} else {
 		res.status(400).render("error", { message: "Something went wrong!" });
 	}
